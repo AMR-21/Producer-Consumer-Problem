629036b4b84cc525438ed959d8064747993b0e7a
amr21 <amryasser52001@gmail.com>
2022-12-16 19:14:57 +0200

    final

---
+++ producersX20.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+sudo pkill producer
+./producer Lead 2000 1 450 15&
+./producer Copper 5100 1.5 350 15&
+./producer Cotton 3000 1.2 510 15&
+./producer Zinc 8200 0.5 530 15&
+./producer CrudeOil 1000 0.8 500 15&
+./producer Aluminium 2500 0.7 230 15&
+./producer Gold 1720 0.11 333 15&
+./producer Silver  2000 0.25 590 15&
+./producer CrudeOil 1100 0.21 340 15&
+./producer Aluminium 4000 0.3 490 15&
+./producer Gold 1300 0.6 644 15&
+./producer Silver 2222 0.45 700 15&
+./producer Nickel 1121 0.88 236 15&
+./producer MENTHAOIL 6000 1.3 800 15&
+./producer Lead 4900 1.8 140 15&
+./producer Cotton 1000 1.6 700 15&
+./producer Nickel 1200 1.1 444 15&
+./producer Gold 3030 1.293 500 15&
+./producer Cotton 7900 2 490 15&
+./producer NAturalgas 7900 2 490 15&
+./producer CrudeOil 2200 0.7 999 15&
